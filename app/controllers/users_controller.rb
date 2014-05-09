38e194e1d14cdf117c524ffc27526d3479c250fc
Suzanne Sanders <suzanne.sanders@gmail.com>
2014-05-09 12:00:57 -0400

    add user show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
-class UsersController < ApplicationController
+	class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   	@pins = @user.pins.page(params[:page]).per_page(20)
   end
 end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Omrails::Application.routes.draw do
+  get "users/show"
   resources :pins
 
   devise_for :users
- #devise_for : users
+  match 'users/:id' => 'users#show', as: :user, via: [:get, :post]
+ 
   devise_scope :user do
   match '/users/sign_out' => 'sessions#destroy', via: [:get, :delete]
 end
